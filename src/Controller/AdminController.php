db7db36b142760f662077ecd1859e06518dac469
IvanR <ivan.ramirezf@gmail.com>
2022-06-21 00:18:43 +1000

    PHP files to twig extension
    * PHP files to twig extension
    * Controller calling admin page

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,15 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/admin")
+ */
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="app_admin")
+     * @Route("/", name="admin_main_page")
      */
     public function index(): Response
     {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
-        ]);
+        return $this->render('admin/my_profile.html.twig');
     }
 }
