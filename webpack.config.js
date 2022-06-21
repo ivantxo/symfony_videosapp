3e6783f5f230fa1390bf45b2003d6fd5abcf0ae3
IvanR <ivan.ramirezf@gmail.com>
2022-06-21 23:33:33 +1000

    Webpack encore
    * npm init
    * npm install @symfony/webpack-encore --save-dev
    * ./node_modules/.bin/encore production or ./node_modules/.bin/encore dev --watch

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+// webpack.config.js
+const Encore = require('@symfony/webpack-encore');
+
+Encore
+    // directory where compiled assets will be stored
+    .setOutputPath('public/assets/')
+    // public path used by the web server to access the output path
+    .setPublicPath('/assets')
+
+    // .addEntry('app', './assets/js/app.js')
+    .addStyleEntry('css/dashboard', ['./assets/css/dashboard.css'])
+    .addStyleEntry('css/login', ['./assets/css/login.css'])
+;
+
+Encore.enableSingleRuntimeChunk()
+
+module.exports = Encore.getWebpackConfig()
