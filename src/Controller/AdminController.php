7f9cac6923531fa38a9fabb595fe0a536509bc36
IvanR <ivan.ramirezf@gmail.com>
2022-06-21 22:43:37 +1000

    Template links working in Symfony
    * php links to html.twig
    * Created admin routes

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -9,13 +9,46 @@ use Symfony\Component\Routing\Annotation\Route;
 /**
  * @Route("/admin")
  */
-class AdminController extends AbstractController
-{
+class AdminController extends AbstractController {
     /**
      * @Route("/", name="admin_main_page")
      */
-    public function index(): Response
-    {
+    public function index(): Response {
         return $this->render('admin/my_profile.html.twig');
     }
+
+	/**
+	 * @Route("/categories", name="categories")
+	 */
+	public function categories(): Response {
+		return $this->render('admin/categories.html.twig');
+	}
+
+	/**
+	 * @Route("/videos", name="videos")
+	 */
+	public function videos(): Response {
+		return $this->render('admin/videos.html.twig');
+	}
+
+	/**
+	 * @Route("/upload_video", name="upload_video")
+	 */
+	public function upload_video(): Response {
+		return $this->render('admin/upload_video.html.twig');
+	}
+
+	/**
+	 * @Route("/users", name="users")
+	 */
+	public function users(): Response {
+		return $this->render('admin/users.html.twig');
+	}
+
+	/**
+	 * @Route("/edit_category", name="edit_category")
+	 */
+	public function edit_category(): Response {
+		return $this->render('admin/edit_category.html.twig');
+	}
 }
