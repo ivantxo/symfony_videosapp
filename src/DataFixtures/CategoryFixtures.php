0b40f296183a28515c8562bbbe9b1c3b6c0cdf84
IvanR <ivan.ramirezf@gmail.com>
2022-06-22 23:40:39 +1000

    Added fixtures for Categories
    * bin/console make:fixtures

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -9,6 +9,7 @@ use Doctrine\Persistence\ObjectManager;
 class CategoryFixtures extends Fixture {
     public function load(ObjectManager $manager): void {
 		$this->load_main_categories($manager);
+		$this->load_subcategories($manager, 'electronics', 1);
     }
 
 	private function load_main_categories(ObjectManager $manager) {
@@ -20,18 +21,32 @@ class CategoryFixtures extends Fixture {
 		$manager->flush();
 	}
 
+	private function load_subcategories(ObjectManager $manager, string $category, int $parent_id) {
+		$parent = $manager->getRepository(Category::class)->find($parent_id);
+		$method_name = "get_{$category}_data";
+		foreach ($this->$method_name() as [$name, $id]) {
+			$category = new Category();
+			$category->setName($name);
+			$category->setParent($parent);
+			$manager->persist($category);
+		}
+		$manager->flush();
+	}
+
 	private function get_main_categories_data(): array {
 		return [
 			['Electronics', 1],
 			['Books', 2],
 			['Movies', 3],
-			['Funny', 4],
-			['For kids', 5],
-			['For adults', 6],
-			['Scary', 7],
-			['Inspirational', 8],
-			['Motivating', 9],
-			['Surprising', 10],
+			['Toys', 4],
+		];
+	}
+
+	private function get_electronics_data(): array {
+		return [
+			['Cameras', 5],
+			['Computers', 6],
+			['Mobile Phones', 7],
 		];
 	}
 }
