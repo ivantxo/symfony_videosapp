4ad15a10cbd183abe062b0aa7f3b7b039500b2a5
IvanR <ivan.ramirezf@gmail.com>
2022-06-23 00:06:45 +1000

    Added Category fixture (fake data)
    * As above

--- migrations/Version20220622140534.php
+++ migrations/Version20220622140534.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220622133956 extends AbstractMigration
+final class Version20220622140534 extends AbstractMigration
 {
     public function getDescription(): string
     {
--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -9,7 +9,12 @@ use Doctrine\Persistence\ObjectManager;
 class CategoryFixtures extends Fixture {
     public function load(ObjectManager $manager): void {
 		$this->load_main_categories($manager);
-		$this->load_subcategories($manager, 'electronics', 1);
+		$this->load_electronics($manager);
+		$this->load_computers($manager);
+		$this->load_laptops($manager);
+		$this->load_books($manager);
+		$this->load_movies($manager);
+		$this->load_romance($manager);
     }
 
 	private function load_main_categories(ObjectManager $manager) {
@@ -19,6 +24,34 @@ class CategoryFixtures extends Fixture {
 			$manager->persist($category);
 		}
 		$manager->flush();
+	}
+
+	private function load_electronics(ObjectManager $manager) {
+		$this->load_subcategories($manager, 'electronics', 1);
+	}
+
+	private function load_computers(ObjectManager $manager) {
+		$this->load_subcategories($manager, 'computers', 6);
+	}
+
+	private function load_laptops($manager)
+	{
+		$this->load_subcategories($manager,'Laptops',8);
+	}
+
+	private function load_books($manager)
+	{
+		$this->load_subcategories($manager,'Books',3);
+	}
+
+	private function load_movies($manager)
+	{
+		$this->load_subcategories($manager,'Movies',4);
+	}
+
+	private function load_romance($manager)
+	{
+		$this->load_subcategories($manager,'Romance',18);
 	}
 
 	private function load_subcategories(ObjectManager $manager, string $category, int $parent_id) {
@@ -36,8 +69,8 @@ class CategoryFixtures extends Fixture {
 	private function get_main_categories_data(): array {
 		return [
 			['Electronics', 1],
-			['Books', 2],
-			['Movies', 3],
+			['Movies', 2],
+			['Books', 3],
 			['Toys', 4],
 		];
 	}
@@ -47,6 +80,47 @@ class CategoryFixtures extends Fixture {
 			['Cameras', 5],
 			['Computers', 6],
 			['Mobile Phones', 7],
+		];
+	}
+
+	private function get_computers_data(): array {
+		return [
+			['Laptops', 8],
+			['Desktops', 9],
+		];
+	}
+
+	private function get_laptops_data(): array {
+		return [
+			['Apple',10],
+			['Asus',11],
+			['Dell',12],
+			['Lenovo',13],
+			['HP',14]
+		];
+	}
+
+
+	private function get_books_data(): array {
+		return [
+			['Children\'s Books',15],
+			['Kindle eBooks',16],
+		];
+	}
+
+
+	private function get_movies_data(): array	{
+		return [
+			['Family',17],
+			['Romance',18],
+		];
+	}
+
+
+	private function get_romance_data(): array {
+		return [
+			['Romantic Comedy',19],
+			['Romantic Drama',20],
 		];
 	}
 }
