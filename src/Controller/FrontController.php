ab5c27e84eaae3a83e7bed96903bf91aff12e62e
IvanR <ivan.ramirezf@gmail.com>
2022-09-24 08:00:57 +1000

    User Registration pt 3

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -9,7 +9,9 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use App\Entity\User;
 use App\Form\UserType;
@@ -88,16 +90,43 @@ class FrontController extends AbstractController {
 	/**
 	 * @Route("/register", name="register")
 	 */
-	public function register(Request $request): Response {
+	public function register(
+		Request $request,
+		UserPasswordHasherInterface $passwordHasher
+	): Response {
 		$user = new User();
 		$form = $this->createForm(UserType::class, $user);
 		$form->handleRequest($request);
+//		dd('register');
+//		dd($form->getErrors());
 		if ($form->isSubmitted() && $form->isValid()) {
-			dd('Register new user');
+//			dd('register_2');
+			$entity_manager = $this->doctrine->getManager();
+			$user->setName($request->request->get('user')['name']);
+			$user->setLastName($request->request->get('user')['last_name']);
+			$user->setEmail($request->request->get('user')['email']);
+			$password = $passwordHasher->hashPassword($user, $request->request->get('user')['password']['first']);
+			$user->setPassword($password);
+			$user->setRoles(['ROLE_USER']);
+			$entity_manager->persist($user);
+			$entity_manager->flush();
+			$this->login_user_automatically($user, $password);
+			return $this->redirectToRoute('admin_main_page');
 		}
 		return $this->render('front/register.html.twig', [
 			'form' => $form->createView(),
 		]);
+	}
+
+	private function login_user_automatically(User $user, string $password) {
+		$token = new UsernamePasswordToken(
+			$user,
+			$password,
+			'main',
+			$user->getRoles()
+		);
+		$this->get('security.token_storage')->setToken($token);
+		$this->get('session')->set(' security main', serialize($token));
 	}
 
 	/**
